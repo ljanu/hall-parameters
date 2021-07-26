a27c704a728ef551652b9d826b33ed59cd48c3e8
ljanu <info@gamatall.cz>
2021-07-26 10:11:28 +0200

    clear code

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -3,9 +3,10 @@
 
         <h1>{{trans('t_h_p.text.list_object')}}</h1>
 
-        {{-- TODO tlačítko zpět, překlad nadpousů, testy, vyzkoušet v jiné apolikaci --}}
-        <p> dokončit krok zpět pomocí back zatím nefunkční</p>
+        {{-- TODO testy, --}}
         <p>seznam zadaných objektů</p>
+        <a href="{{route(trans('t_h_p.routes.h_p_show'), '2')}}">Objekt id: 2</a>
+
 
     </x-slot>
 </x-hall-parameters::welcome_hall_parameters>
