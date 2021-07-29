f5201aaee4aee068c21a9ea690c79024cb6f00ad
ljanu <info@gamatall.cz>
2021-07-29 14:44:18 +0200

    complete base url construction and component icon

--- src/views/components/welcome_hall_parameters.blade.php
+++ src/views/components/welcome_hall_parameters.blade.php
@@ -5,7 +5,6 @@
 
            <x-hall-parameters::html.back/>
 
-
            {{-- flash --}}
            @if ($message = Session::get('success'))
                <div class="m-10 p-2 text-green-500 text-center bg-white dark:bg-gray-700 ">
