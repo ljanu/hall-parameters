c5303790b2a847f5cfd7534469336eab5fddf7a9
ljanu <info@gamatall.cz>
2021-07-29 11:07:01 +0200

    update code

--- src/views/components/welcome_hall_parameters.blade.php
+++ src/views/components/welcome_hall_parameters.blade.php
@@ -1,17 +1,19 @@
 <x-dynamic-component :component="config('config_hall.layout_name')">
     <x-slot :name="config('config_hall.slot_name')">
 
-        <x-hall-parameters::html.back/>
+       <div class="border-t border-red-500">
+           <x-hall-parameters::html.back/>
 
 
-        {{-- flash --}}
-        @if ($message = Session::get('success'))
-            <div class="m-10 p-2 text-green-500 text-center bg-white dark:bg-gray-700 ">
-                <strong>{{ $message }}</strong>
-            </div>
-        @endif
+           {{-- flash --}}
+           @if ($message = Session::get('success'))
+               <div class="m-10 p-2 text-green-500 text-center bg-white dark:bg-gray-700 ">
+                   <strong>{{ $message }}</strong>
+               </div>
+           @endif
 
-        {{$content }}
+           {{$content }}
+       </div>
 
     </x-slot>
 </x-dynamic-component>
