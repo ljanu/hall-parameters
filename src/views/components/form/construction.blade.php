94bf8945e71ef433cde0d5a9ac09bd16bd4b8dac
ljanu <info@gamatall.cz>
2021-07-31 12:57:46 +0200

    form construction with img

--- src/views/components/form/construction.blade.php
+++ src/views/components/form/construction.blade.php
@@ -3,85 +3,139 @@
     'method' => $method
 ])
 
-<div class="mt-10 py-5 flex justify-center text-center bg-gray-200 dark:bg-gray-800">
-    {{-- form --}}
-    <form action="{{route('construction.store')}}" id="form-construction" method="post" class="max-w-3xl">
-        @csrf
-        @method($method)
+{{-- component wraper --}}
+<div class="mt-5 md:mt-10 bg-gray-100 dark:bg-gray-800">
+
+
+    {{-- img dimension--}}
+    <div class="flex flex-col items-center md:flex-row md:justify-center ">
+        {{-- img --}}
+        <img class="mt-5" src="{{asset('images/hall-parameters/gable-roof-dimension.svg')}}" width="200" height="200"
+             alt="gable roof dimension">
+        <img class="mt-5" src="{{asset('images/hall-parameters/flat-roof-dimension.svg')}}" width="200" height="200"
+             alt="gable roof dimension">
+    </div>
+
+    {{-- form content wraper --}}
+    <div class="py-5 flex justify-center text-center ">
+
+        {{-- form --}}
+        <form action="{{route('construction.store')}}" id="form-construction" method="post" class="max-w-3xl">
+            @csrf
+            @method($method)
 
 
 
-        {{-- dimension --}}
-        <fieldset class="mt-10 py-10 ">
-            <div class="mb-5 text-blue-700 dark:text-blue-300">rozměry</div>
+            {{-- dimension -------------------------------------------------------------------------------------------}}
+            <fieldset class="py-10 ">
+                <div class="mb-5 text-blue-700 dark:text-blue-300">rozměry</div>
 
-            <div class="flex flex-col md:flex-row ">
-                <div>
-                    <label for="width">šířka</label>
-                    <input type="text" id="width" name="width" autofocus class="w-28" placeholder="šiřka (m)" value="">
+                {{-- dimension --}}
+                <div class="flex flex-col items-center md:flex-row ">
+
+                    {{-- width --}}
+                    <div class="flex flex-row">
+                        <label for="width">
+                            <img src="{{asset('images/hall-parameters/width.svg')}}" alt="width">
+                        </label>
+                        <input type="text" id="width" name="width" autofocus class="w-28" placeholder="šiřka (m)"
+                               value="">
+                    </div>
+
+                    {{-- lenght --}}
+                    <div class="flex flex-row">
+                        <label for="length">
+                            <img src="{{asset('images/hall-parameters/length.svg')}}" alt="length">
+                        </label>
+                        <input type="text" id="length" name="length" class="w-28" placeholder="délka (m)" value="">
+                    </div>
+
+                    {{-- height --}}
+                    <div class="flex flex-row">
+                        <label for="height">
+                            <img src="{{asset('images/hall-parameters/height.svg')}}" alt="height">
+                        </label>
+                        <input type="text" id="height" name="height" class="w-28" placeholder="výška (m)" value="">
+                    </div>
+
                 </div>
 
-                <div>
-                    <label for="length">délka</label>
-                    <input type="text" id="length" name="length" class="w-28" placeholder="délka (m)" value="">
+            </fieldset>
+
+
+            {{-- rooof type ------------------------------------------------------------------------------------------}}
+            <fieldset class="mt-14">
+
+                <div class="mb-5 text-blue-700 dark:text-blue-300">typ střechy</div>
+
+                {{-- roof type --}}
+                <div class="flex flex-row justify-center">
+
+                    {{-- gable roof --}}
+                    <div>
+                        <label for="gable_roof" class="text-left self-center">
+                            <img src="{{asset('images/hall-parameters/gable-roof.svg')}}" alt="gable roof">
+                        </label>
+                        <input class="self-center" type="radio" id="gable_roof" name="roof_type" value="gable_roof"
+                               checked="checked">
+                    </div>
+
+                    {{-- flat  roof left --}}
+                    <div class="mx-5">
+                        <label for="flat_roof_left" class="text-right self-center">
+                            <img src="{{asset('images/hall-parameters/flat-roof-left.svg')}}" alt="flat roof left">
+                        </label>
+                        <input class="self-center" type="radio" id="flat_roof_left" name="roof_type"
+                               value="flat_roof_left">
+                    </div>
+
+                    {{-- flar roof right --}}
+                    <div>
+                        <label for="flat_roof_right" class="text-right self-center">
+                            <img src="{{asset('images/hall-parameters/flat-roof-right.svg')}}" alt="flat roof left">
+                        </label>
+                        <input class="self-center" type="radio" id="flat_roof_right" name="roof_type"
+                               value="flat_roof_right">
+                    </div>
+
                 </div>
 
-                <div>
-                    <label for="height">výška</label>
-                    <input type="text" id="height" name="height" class="w-28" placeholder="výška (m)" value="">
+            </fieldset>
+
+
+            {{-- limitations -----------------------------------------------------------------------------------------}}
+            <fieldset class="mt-10">
+                <div class="mb-5 text-blue-700 dark:text-blue-300">omezení</div>
+
+                {{-- limitations --}}
+                <div class="grid grid-cols-2">
+
+                    {{-- roof pitch --}}
+                    <label for="roof_pitch" class="text-right self-center">sklon střechy</label>
+                    <input type="text" id="roof_pitch" name="roof_pitch" class="w-28" value="">
+
+                    {{-- max module --}}
+                    <label for="max_module" class="text-right self-center">max rozměr modulu</label>
+                    <input type="text" id="max_module" name="max_module" class="w-28" value="">
+
+                    {{-- max gap purlin --}}
+                    <label for="max_gap_purlin" class="text-right self-center">max rozměr vaznic</label>
+                    <input type="text" id="max_gap_purlin" name="max_gap_purlin" class="w-28" value="">
+
+                    {{-- max gap side rails --}}
+                    <label for="max_gap_side_rails" class="text-right self-center">max rozměr stěnových vazniček</label>
+                    <input type="text" id="max_gap_side_rails" name="max_gap_side_rails" class="w-28" value="">
+
                 </div>
-            </div>
 
-        </fieldset>
-
-
-        {{-- rooof --}}
-        <fieldset class="mt-14">
-
-            <div class="mb-5 text-blue-700 dark:text-blue-300">střecha</div>
-
-            {{-- foof type --}}
-            <div class="grid grid-cols-2">
-                <label for="gable_roof" class="text-right self-center">sedlová střecha</label>
-                <input class="self-center" type="radio" id="gable_roof" name="roof_type" value="gable_roof" checked="checked">
-
-                <label for="flat_roof_right" class="text-right self-center">pultová střecha do prava</label>
-                <input class="self-center"  type="radio" id="flat_roof_right" name="roof_type" value="flat_roof_right">
-
-                <label for="flat_roof_left" class="text-right self-center">pultová střecha do leva</label>
-                <input class="self-center"  type="radio" id="flat_roof_left" name="roof_type" value="flat_roof_left">
-            </div>
-
-            {{-- roof pitch --}}
-            <label for="roof_pitch">sklon střechy</label>
-            <input type="text" id="roof_pitch" name="roof_pitch" class="w-28" value="">
-
-        </fieldset>
+            </fieldset>
 
 
-        {{-- limitations --}}
-        <fieldset class="mt-10">
-            <div class="mb-5 text-blue-700 dark:text-blue-300">omezení</div>
+            {{-- button --}}
+            <button class="my-10" type="submit">{{trans("t_h_p.text.save")}}</button>
 
-
-            <div class="grid grid-cols-2">
-                <label for="max_model" class="text-right self-center">max rozměr modelu</label>
-                <input type="text" id="max_model" name="max_model" class="w-28"  value="">
-
-                <label for="max_gap_purlin" class="text-right self-center">max rozměr vaznic</label>
-                <input type="text" id="max_gap_purlin" name="max_gap_purlin" class="w-28" value="">
-
-                <label for="max_gap_side_rails" class="text-right self-center">max rozměr stěnových vazniček</label>
-                <input type="text" id="max_gap_side_rails" name="max_gap_side_rails" class="w-28" value="">
-            </div>
-
-        </fieldset>
-
-
-        {{-- button --}}
-        <button class="my-10" type="submit">{{trans("t_h_p.text.save")}}</button>
-
-    </form>
+        </form>
+    </div>
 </div>
 
 
