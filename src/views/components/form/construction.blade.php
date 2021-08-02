198cbb01b5023a95b106895f85ac4c0089281281
ljanu <info@gamatall.cz>
2021-08-02 11:47:54 +0200

    translate text construction

--- src/views/components/form/construction.blade.php
+++ src/views/components/form/construction.blade.php
@@ -28,7 +28,7 @@
 
             {{-- dimension -------------------------------------------------------------------------------------------}}
             <fieldset class="py-10 ">
-                <div class="m-title">rozměry</div>
+                <div class="m-title">{{trans("t_h_p.text.dimension")}}</div>
 
                 {{-- dimension --}}
                 <div class="flex flex-col items-center md:flex-row ">
@@ -36,26 +36,26 @@
                     {{-- width --}}
                     <div class="flex flex-row">
                         <label for="width">
-                            <img src="{{asset('images/hall-parameters/width.svg')}}" alt="width">
+                            <img src="{{asset('images/hall-parameters/width.svg')}}" alt="{{trans("t_h_p.text.width")}}">
                         </label>
-                        <input type="text" id="width" name="width" autofocus class="w-28" placeholder="šiřka (m)"
+                        <input type="text" id="width" name="width" autofocus class="w-28" placeholder="{{trans("t_h_p.text.width")}} (m)"
                                value="">
                     </div>
 
                     {{-- lenght --}}
                     <div class="flex flex-row">
                         <label for="length">
-                            <img src="{{asset('images/hall-parameters/length.svg')}}" alt="length">
+                            <img src="{{asset('images/hall-parameters/length.svg')}}" alt="{{trans("t_h_p.text.length")}}">
                         </label>
-                        <input type="text" id="length" name="length" class="w-28" placeholder="délka (m)" value="">
+                        <input type="text" id="length" name="length" class="w-28" placeholder="{{trans("t_h_p.text.length")}} (m)" value="">
                     </div>
 
                     {{-- height --}}
                     <div class="flex flex-row">
                         <label for="height">
-                            <img src="{{asset('images/hall-parameters/height.svg')}}" alt="height">
+                            <img src="{{asset('images/hall-parameters/height.svg')}}" alt="{{trans("t_h_p.text.height")}}">
                         </label>
-                        <input type="text" id="height" name="height" class="w-28" placeholder="výška (m)" value="">
+                        <input type="text" id="height" name="height" class="w-28" placeholder="{{trans("t_h_p.text.height")}} (m)" value="">
                     </div>
 
                 </div>
@@ -66,7 +66,7 @@
             {{-- rooof type ------------------------------------------------------------------------------------------}}
             <fieldset class="mt-14">
 
-                <div class="m-title">typ střechy</div>
+                <div class="m-title">{{trans("t_h_p.text.roof_type")}}</div>
 
                 {{-- roof type --}}
                 <div class="flex flex-row justify-center">
@@ -74,7 +74,7 @@
                     {{-- gable roof --}}
                     <div>
                         <label for="gable_roof" class="text-left self-center">
-                            <img src="{{asset('images/hall-parameters/gable-roof.svg')}}" alt="gable roof">
+                            <img src="{{asset('images/hall-parameters/gable-roof.svg')}}" alt="{{trans("t_h_p.text.gable_roof")}}">
                         </label>
                         <input class="self-center" type="radio" id="gable_roof" name="roof_type" value="gable_roof"
                                checked="checked">
@@ -83,7 +83,7 @@
                     {{-- flat  roof left --}}
                     <div class="mx-5">
                         <label for="flat_roof_left" class="text-right self-center">
-                            <img src="{{asset('images/hall-parameters/flat-roof-left.svg')}}" alt="flat roof left">
+                            <img src="{{asset('images/hall-parameters/flat-roof-left.svg')}}" alt="{{trans("t_h_p.text.flat_roof_left")}}">
                         </label>
                         <input class="self-center" type="radio" id="flat_roof_left" name="roof_type"
                                value="flat_roof_left">
@@ -92,7 +92,7 @@
                     {{-- flar roof right --}}
                     <div>
                         <label for="flat_roof_right" class="text-right self-center">
-                            <img src="{{asset('images/hall-parameters/flat-roof-right.svg')}}" alt="flat roof left">
+                            <img src="{{asset('images/hall-parameters/flat-roof-right.svg')}}" alt="{{trans("t_h_p.text.flat_roof_right")}}">
                         </label>
                         <input class="self-center" type="radio" id="flat_roof_right" name="roof_type"
                                value="flat_roof_right">
@@ -105,25 +105,25 @@
 
             {{-- limitations -----------------------------------------------------------------------------------------}}
             <fieldset class="mt-10">
-                <div class="m-title">omezení</div>
+                <div class="m-title">{{trans("t_h_p.text.limitations")}}</div>
 
                 {{-- limitations --}}
                 <div class="grid grid-cols-2">
 
                     {{-- roof pitch --}}
-                    <label for="roof_pitch" class="text-right self-center">sklon střechy</label>
+                    <label for="roof_pitch" class="text-right self-center">{{trans("t_h_p.text.roof_pitch")}}</label>
                     <input type="text" id="roof_pitch" name="roof_pitch" class="w-28" value="">
 
                     {{-- max module --}}
-                    <label for="max_module" class="text-right self-center">max rozměr modulu</label>
+                    <label for="max_module" class="text-right self-center">{{trans("t_h_p.text.max_module_size")}}</label>
                     <input type="text" id="max_module" name="max_module" class="w-28" value="">
 
                     {{-- max gap purlin --}}
-                    <label for="max_gap_purlin" class="text-right self-center">max rozměr vaznic</label>
+                    <label for="max_gap_purlin" class="text-right self-center">{{trans("t_h_p.text.max_purlin_size")}}</label>
                     <input type="text" id="max_gap_purlin" name="max_gap_purlin" class="w-28" value="">
 
                     {{-- max gap side rails --}}
-                    <label for="max_gap_side_rails" class="text-right self-center">max rozměr stěnových vazniček</label>
+                    <label for="max_gap_side_rails" class="text-right self-center">{{trans("t_h_p.text.max_side_rails_size")}}</label>
                     <input type="text" id="max_gap_side_rails" name="max_gap_side_rails" class="w-28" value="">
 
                 </div>
