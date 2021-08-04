b36d1cda849aaa857f355d59486268724987b38a
ljanu <info@gamatall.cz>
2021-08-04 09:49:40 +0200

    code reformat

--- src/Http/Controllers/Specification/ConstructionController.php
+++ src/Http/Controllers/Specification/ConstructionController.php
@@ -27,7 +27,7 @@ class  ConstructionController extends Controller
 
     public function store(Request $request)
     {
-      //  ddd($request->all());
+      return $request;
     }
 
 
--- src/views/components/form/construction.blade.php
+++ src/views/components/form/construction.blade.php
@@ -4,139 +4,246 @@
 ])
 
 {{-- component wraper --}}
-<div class="mt-5 md:mt-10 bg-gray-100 dark:bg-gray-800">
+<div class = "mt-5 md:mt-10 bg-gray-100 dark:bg-gray-800" >
 
 
     {{-- img dimension--}}
-    <div class="flex flex-col items-center md:flex-row md:justify-center ">
+    <div class = "flex flex-col items-center md:flex-row md:justify-center " >
         {{-- img --}}
-        <img class="mt-5" src="{{asset('images/hall-parameters/gable-roof-dimension.svg')}}" width="200" height="200"
-             alt="gable roof dimension">
-        <img class="mt-5" src="{{asset('images/hall-parameters/flat-roof-dimension.svg')}}" width="200" height="200"
-             alt="gable roof dimension">
-    </div>
+        <img
+                class = "mt-5"
+                src = "{{asset('images/hall-parameters/gable-roof-dimension.svg')}}"
+                width = "200"
+                height = "200"
+                alt = "gable roof dimension" >
+        <img
+                class = "mt-5"
+                src = "{{asset('images/hall-parameters/flat-roof-dimension.svg')}}"
+                width = "200"
+                height = "200"
+                alt = "gable roof dimension" >
+    </div >
 
     {{-- form content wraper --}}
-    <div class="py-5 flex justify-center text-center ">
+    <div class = "py-5 flex justify-center text-center " >
 
         {{-- form --}}
-        <form action="{{route('construction.store')}}" id="form-construction" method="post" class="max-w-3xl">
+        <form
+                action = "{{route('construction.store')}}"
+                id = "form-construction"
+                method = "post" >
             @csrf
             @method($method)
 
 
 
             {{-- dimension -------------------------------------------------------------------------------------------}}
-            <fieldset class="py-10 ">
-                <div class="m-title">{{trans("t_h_p.text.dimension")}}</div>
+            <fieldset class = "py-10 " >
+                <div class = "m-title" >{{trans("t_h_p.text.dimension")}}</div >
 
                 {{-- dimension --}}
-                <div class="flex flex-col items-center md:flex-row ">
+                <div class = "flex flex-col items-center md:flex-row " >
 
                     {{-- width --}}
-                    <div class="flex flex-row">
-                        <label for="width">
-                            <img src="{{asset('images/hall-parameters/width.svg')}}" alt="{{trans("t_h_p.text.width")}}">
-                        </label>
-                        <input type="text" id="width" name="width" autofocus class="w-28" placeholder="{{trans("t_h_p.text.width")}} (m)"
-                               value="">
-                    </div>
+                    <div class = "flex flex-row" >
+
+                        <label for = "width" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/width.svg')}}"
+                                    alt = "{{trans("t_h_p.text.width")}}"
+                            >
+                        </label >
+
+                        <input
+                                type = "number"
+                                id = "width"
+                                name = "width"
+                                autofocus
+                                class = "w-36"
+                                placeholder = "{{trans("t_h_p.text.width")}} (m)"
+                                value = ""
+                                step = "0.01"
+                        >
+
+                    </div >
 
                     {{-- lenght --}}
-                    <div class="flex flex-row">
-                        <label for="length">
-                            <img src="{{asset('images/hall-parameters/length.svg')}}" alt="{{trans("t_h_p.text.length")}}">
-                        </label>
-                        <input type="text" id="length" name="length" class="w-28" placeholder="{{trans("t_h_p.text.length")}} (m)" value="">
-                    </div>
+                    <div class = "flex flex-row" >
+
+                        <label for = "length" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/length.svg')}}"
+                                    alt = "{{trans("t_h_p.text.length")}}"
+                            >
+                        </label >
+
+                        <input
+                                type = "number"
+                                id = "length"
+                                name = "length"
+                                class = "w-36"
+                                placeholder = "{{trans("t_h_p.text.length")}} (m)"
+                                value = ""
+                                step = "0.01"
+                        >
+
+                    </div >
 
                     {{-- height --}}
-                    <div class="flex flex-row">
-                        <label for="height">
-                            <img src="{{asset('images/hall-parameters/height.svg')}}" alt="{{trans("t_h_p.text.height")}}">
-                        </label>
-                        <input type="text" id="height" name="height" class="w-28" placeholder="{{trans("t_h_p.text.height")}} (m)" value="">
-                    </div>
+                    <div class = "flex flex-row" >
+                        <label for = "height" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/height.svg')}}"
+                                    alt = "{{trans("t_h_p.text.height")}}" >
+                        </label >
 
-                </div>
+                        <input
+                                type = "number"
+                                id = "height"
+                                name = "height"
+                                class = "w-36"
+                                placeholder = "{{trans("t_h_p.text.height")}} (m)"
+                                value = ""
+                                step = "0.01" >
+                    </div >
 
-            </fieldset>
+                </div >
+
+            </fieldset >
 
 
             {{-- rooof type ------------------------------------------------------------------------------------------}}
-            <fieldset class="mt-14">
+            <fieldset class = "mt-14" >
 
-                <div class="m-title">{{trans("t_h_p.text.roof_type")}}</div>
+                <div class = "m-title" >{{trans("t_h_p.text.roof_type")}}</div >
 
                 {{-- roof type --}}
-                <div class="flex flex-row justify-center">
+                <div class = "flex flex-row justify-center" >
 
                     {{-- gable roof --}}
-                    <div>
-                        <label for="gable_roof" class="text-left self-center">
-                            <img src="{{asset('images/hall-parameters/gable-roof.svg')}}" alt="{{trans("t_h_p.text.gable_roof")}}">
-                        </label>
-                        <input class="self-center" type="radio" id="gable_roof" name="roof_type" value="gable_roof"
-                               checked="checked">
-                    </div>
+                    <div >
+                        <label
+                                for = "gable_roof"
+                                class = "text-left self-center" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/gable-roof.svg')}}"
+                                    alt = "{{trans("t_h_p.text.gable_roof")}}" >
+                        </label >
+
+                        <input
+                                type = "radio"
+                                id = "gable_roof"
+                                class = "self-center"
+                                name = "roof_type"
+                                value = "gable_roof"
+                                checked = "checked" >
+                    </div >
 
                     {{-- flat  roof left --}}
-                    <div class="mx-5">
-                        <label for="flat_roof_left" class="text-right self-center">
-                            <img src="{{asset('images/hall-parameters/flat-roof-left.svg')}}" alt="{{trans("t_h_p.text.flat_roof_left")}}">
-                        </label>
-                        <input class="self-center" type="radio" id="flat_roof_left" name="roof_type"
-                               value="flat_roof_left">
-                    </div>
+                    <div class = "mx-5" >
+                        <label
+                                for = "flat_roof_left"
+                                class = "text-right self-center" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/flat-roof-left.svg')}}"
+                                    alt = "{{trans("t_h_p.text.flat_roof_left")}}" >
+                        </label >
+                        <input
+                                type = "radio"
+                                id = "flat_roof_left"
+                                class = "self-center"
+                                name = "roof_type"
+                                value = "flat_roof_left" >
+                    </div >
 
-                    {{-- flar roof right --}}
-                    <div>
-                        <label for="flat_roof_right" class="text-right self-center">
-                            <img src="{{asset('images/hall-parameters/flat-roof-right.svg')}}" alt="{{trans("t_h_p.text.flat_roof_right")}}">
-                        </label>
-                        <input class="self-center" type="radio" id="flat_roof_right" name="roof_type"
-                               value="flat_roof_right">
-                    </div>
+                    {{-- flat roof right --}}
+                    <div >
+                        <label
+                                for = "flat_roof_right"
+                                class = "text-right self-center" >
+                            <img
+                                    src = "{{asset('images/hall-parameters/flat-roof-right.svg')}}"
+                                    alt = "{{trans("t_h_p.text.flat_roof_right")}}" >
+                        </label >
+                        <input
+                                id = "flat_roof_right"
+                                class = "self-center"
+                                type = "radio"
+                                name = "roof_type"
+                                value = "flat_roof_right" >
+                    </div >
 
-                </div>
+                </div >
 
-            </fieldset>
+            </fieldset >
 
 
             {{-- limitations -----------------------------------------------------------------------------------------}}
-            <fieldset class="mt-10">
-                <div class="m-title">{{trans("t_h_p.text.limitations")}}</div>
+            <fieldset class = "mt-10" >
+                <div class = "m-title" >{{trans("t_h_p.text.limitations")}}</div >
 
                 {{-- limitations --}}
-                <div class="grid grid-cols-2">
+                <div class = "grid grid-cols-2" >
 
                     {{-- roof pitch --}}
-                    <label for="roof_pitch" class="text-right self-center">{{trans("t_h_p.text.roof_pitch")}}</label>
-                    <input type="text" id="roof_pitch" name="roof_pitch" class="w-28" value="">
+                    <label
+                            for = "roof_pitch"
+                            class = "text-right self-center" >{{trans("t_h_p.text.roof_pitch")}}</label >
+                    <input
+                            type = "number"
+                            id = "roof_pitch"
+                            name = "roof_pitch"
+                            class = "w-36"
+                            value = "" >
 
                     {{-- max module --}}
-                    <label for="max_module" class="text-right self-center">{{trans("t_h_p.text.max_module_size")}}</label>
-                    <input type="text" id="max_module" name="max_module" class="w-28" value="">
+                    <label
+                            for = "max_module"
+                            class = "text-right self-center" >{{trans("t_h_p.text.max_module_size")}}</label >
+                    <input
+                            type = "number"
+                            id = "max_module"
+                            name = "max_module"
+                            class = "w-36"
+                            value = "" >
 
                     {{-- max gap purlin --}}
-                    <label for="max_gap_purlin" class="text-right self-center">{{trans("t_h_p.text.max_purlin_size")}}</label>
-                    <input type="text" id="max_gap_purlin" name="max_gap_purlin" class="w-28" value="">
+                    <label
+                            for = "max_gap_purlin"
+                            class = "text-right self-center" >{{trans("t_h_p.text.max_purlin_size")}}</label >
+                    <input
+                            type = "number"
+                            id = "max_gap_purlin"
+                            name = "max_gap_purlin"
+                            class = "w-36"
+                            value = ""
+                            step = "0.01" >
 
                     {{-- max gap side rails --}}
-                    <label for="max_gap_side_rails" class="text-right self-center">{{trans("t_h_p.text.max_side_rails_size")}}</label>
-                    <input type="text" id="max_gap_side_rails" name="max_gap_side_rails" class="w-28" value="">
+                    <label
+                            for = "max_gap_side_rails"
+                            class = "text-right self-center" >{{trans("t_h_p.text.max_side_rails_size")}}</label >
+                    <input
+                            type = "number"
+                            id = "max_gap_side_rails"
+                            name = "max_gap_side_rails"
+                            class = "w-36"
+                            value = ""
+                            step = "0.01" >
 
-                </div>
+                </div >
 
-            </fieldset>
+            </fieldset >
 
 
             {{-- button --}}
-            <button class="my-10" type="submit">{{trans("t_h_p.text.save")}}</button>
+            <button
+                    class = "my-10"
+                    type = "submit" >{{trans("t_h_p.text.save")}}</button >
 
-        </form>
-    </div>
-</div>
+        </form >
+    </div >
+</div >
 
 
 
