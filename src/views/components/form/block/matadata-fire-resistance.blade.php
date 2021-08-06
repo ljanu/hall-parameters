09f98b4048e35827c7929fbe64a09cc0151fbc40
ljanu <info@gamatall.cz>
2021-08-06 12:54:30 +0200

    create translator metadata

--- src/views/components/form/block/matadata-fire-resistance.blade.php
+++ src/views/components/form/block/matadata-fire-resistance.blade.php
@@ -1,14 +1,14 @@
 {{-- component fire resistance --}}
 
 <fieldset class="m-base" >
-    <div class = "m-title" >fire resistance</div >
+    <div class = "m-title" >{{trans("t_h_p.text.fire_resistance")}}</div >
 
     <div class = "m-base grid grid-cols-2 self-center" >
 
         {{--  fire resistance construction --}}
         <label
                 for = "fire_resistance_construction"
-                class = "text-right self-center" > fire resistance construction
+                class = "text-right self-center" > {{trans("t_h_p.text.fire_resistance_construction")}}
         </label >
 
         <input
@@ -20,19 +20,35 @@
                 value = ""
         >
 
-        {{--  fire resistance cladding --}}
+        {{--  fire resistance roof --}}
         <label
-                for = "fire_resistance_construction"
-                class = "text-right self-center" > fire resistance cladding
+                for = "fire_resistance_roof"
+                class = "text-right self-center" > {{trans("t_h_p.text.fire_resistance_roof")}}
         </label >
 
         <input
                 type = "number"
-                id = "fire_resistance_construction"
-                name = "fire_resistance_construction"
+                id = "fire_resistance_roof"
+                name = "fire_resistance_roof"
                 autofocus
                 class = "w-56"
                 value = ""
         >
+
+        {{--  fire resistance walls --}}
+        <label
+                for = "fire_resistance_walls"
+                class = "text-right self-center" > {{trans("t_h_p.text.fire_resistance_walls")}}
+        </label >
+
+        <input
+                type = "number"
+                id = "fire_resistance_walls"
+                name = "fire_resistance_walls"
+                autofocus
+                class = "w-56"
+                value = ""
+        >
+
     </div >
 </fieldset >
