0bb3e4cdb8af543eb2e43bcad77fb1c7572298d1
ljanu <info@gamatall.cz>
2021-08-07 09:55:12 +0200

    update method

--- tests/Feature/Cs/ConstructionRoutesTest.php
+++ tests/Feature/Cs/ConstructionRoutesTest.php
@@ -13,7 +13,7 @@ class ConstructionRoutesTest extends TestCase
     public function test_route_get_construction_create_cs()
     {
         $response = $this->get('/hala-parametry/konstrukce/vytvorit');
-        $response ->assertViewIs('hall-parameters::form.construction.create');
+        $response ->assertViewIs('hall-parameters::construction.create');
         $response->assertStatus(200);
     }
 
@@ -24,33 +24,9 @@ class ConstructionRoutesTest extends TestCase
     public function test_route_get_construction_edit_cs()
     {
         $response = $this->get('/hala-parametry/konstrukce/1/upravit');
-        $response->assertViewIs('hall-parameters::form.construction.edit');
+        $response->assertViewIs('hall-parameters::construction.edit');
         $response->assertStatus(200);
     }
 
-
-
-
-
-    //method post ---------------------------------------------------------
-
-    public function test_route_post_construction_store_cs()
-    {
-        $response = $this->post('/hala-parametry/konstrukce');
-
-        $response->assertStatus(200);
-    }
-
-
-
-
-
-    //methods put ----------------------------------------------------
-    public function test_route_put_construction_update_cs()
-    {
-        $response = $this->put('/hala-parametry/konstrukce/1');
-
-        $response->assertStatus(200);
-    }
 
 }
