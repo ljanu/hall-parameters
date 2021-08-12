f318f9ed629c9e157a9d60f21e2b6a379d0fb67a
ljanu <info@gamatall.cz>
2021-08-12 12:19:47 +0200

    completed form contact

--- src/views/components/form/block/contact/address.blade.php
+++ src/views/components/form/block/contact/address.blade.php
@@ -1,6 +1,78 @@
 {{-- component address contact --}}
 
-<fieldset class="m-base" >
-    <legend class="m-title" >Adresa</legend >
+<fieldset class="m-base grid grid-cols-2" >
+    <legend class="m-title" >{{trans("t_h_p.text.address")}}</legend >
+
+    {{-- address line 1 --}}
+    <label class="text-right self-center" for = "address_line_1" > {{trans("t_h_p.text.address_line_1")}}</label >
+
+    <input
+            type = "text"
+            name = "address_line_1"
+            id = "address_line_1"
+    >
+
+    {{-- address line 2 --}}
+    <label class="text-right self-center" for = "address_line_2" > {{trans("t_h_p.text.address_line_2")}}</label >
+
+    <input
+            type = "text"
+            name = "address_line_2"
+            id = "address_line_2"
+    >
+
+    {{-- city --}}
+    <label class="text-right self-center" for = "city" > {{trans("t_h_p.text.city")}}</label >
+
+    <input
+            type = "text"
+            name = "city"
+            id = "city"
+    >
+
+    {{-- zip code --}}
+    <label class="text-right self-center" for = "zip_code" > {{trans("t_h_p.text.zip_code")}}</label >
+
+    <input
+            type = "text"
+            name = "zip_code"
+            id = "zip_code"
+    >
+
+    {{-- region --}}
+    <label class="text-right self-center" for = "region" > {{trans("t_h_p.text.region")}}</label >
+
+    <input
+            type = "text"
+            name = "region"
+            id = "region"
+    >
+
+    {{-- distance --}}
+    <label class="text-right self-center" for = "distance" > {{trans("t_h_p.text.distance")}}</label >
+
+    <input
+            type = "text"
+            name = "distance"
+            id = "distance"
+    >
+
+    {{-- plot number --}}
+    <label class="text-right self-center" for = "plot_number" > {{trans("t_h_p.text.plot_number")}}</label >
+
+    <input
+            type = "text"
+            name = "plot_number"
+            id = "plot_number"
+    >
+
+    {{-- country --}}
+    <label class="text-right self-center" for = "country" > {{trans("t_h_p.text.country")}}</label >
+
+    <input
+            type = "text"
+            name = "country"
+            id = "country"
+    >
 
 </fieldset >
--- src/views/components/form/block/contact/names.blade.php
+++ src/views/components/form/block/contact/names.blade.php
@@ -1,6 +1,52 @@
 {{-- component names contact --}}
 
-<fieldset class = "m-base" >
+<fieldset class = "m-base grid grid-cols-2" >
     <legend class = "m-title" >{{trans("t_h_p.text.contact")}}</legend >
+
+    {{-- company --}}
+    <label class="text-right self-center" for = "company" > {{trans("t_h_p.text.company")}}</label >
+
+    <input
+            type = "text"
+            name = "company"
+            id = "company"
+    >
+
+    {{-- first name --}}
+    <label class="text-right self-center" for = "first_name" > {{trans("t_h_p.text.first_name")}}</label >
+
+    <input
+            type = "text"
+            name = "first_name"
+            id = "first_name"
+    >
+
+    {{-- lasta name --}}
+    <label class="text-right self-center" for = "last_name" > {{trans("t_h_p.text.last_name")}}</label >
+
+    <input
+            type = "text"
+            name = "last_name"
+            id = "last_name"
+    >
+
+    {{-- email --}}
+    <label class="text-right self-center" for = "email" > {{trans("t_h_p.text.email")}}</label >
+
+    <input
+            type = "email"
+            name = "email"
+            id = "email"
+    >
+
+    {{-- phone --}}
+    <label class="text-right self-center" for = "phone" > {{trans("t_h_p.text.phone")}}</label >
+
+    <input
+            type = "text"
+            name = "phone"
+            id = "phone"
+    >
+
 
 </fieldset >
