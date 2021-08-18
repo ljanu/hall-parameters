6314999c68f11befe495ef24782faeb98e91c95b
ljanu <info@gamatall.cz>
2021-08-18 12:33:45 +0200

    added components

--- src/views/components/form/block/specification/cladding-orientation.blade.php
+++ src/views/components/form/block/specification/cladding-orientation.blade.php
@@ -1,47 +1,51 @@
 {{-- component cladding orientation --}}
+<x-hall-parameters::helpers.form-fieldset
+        :legend="trans('t_h_p.text.cladding_orientation')"
+>
 
-<fieldset class = "m-base" >
+    <x-slot name="content">
 
-    <legend class = "m-title" >{{trans("t_h_p.text.cladding_orientation")}}</legend >
+        {{-- orintation facade profile --}}
+        <div class = "flex flex-row justify-center" >
 
-    {{-- orintation facade profile --}}
-    <div class = "flex flex-row justify-center" >
+            <div >
+                <label
+                        for = "horizontal"
+                        class = "text-left self-center" >
+                    <img
+                            src = "{{asset('images/hall-parameters/horizontal.svg')}}"
+                            alt = "{{trans("t_h_p.text.gable_roof")}}" >
+                </label >
 
-        <div >
-            <label
-                    for = "horizontal"
-                    class = "text-left self-center" >
-                <img
-                        src = "{{asset('images/hall-parameters/horizontal.svg')}}"
-                        alt = "{{trans("t_h_p.text.gable_roof")}}" >
-            </label >
+                <input
+                        type = "radio"
+                        id = "horizontal"
+                        class = "self-center"
+                        name = "cladding_orientation"
+                        value = "horizontal"
+                        checked = "checked" >
+            </div >
 
-            <input
-                    type = "radio"
-                    id = "horizontal"
-                    class = "self-center"
-                    name = "cladding_orientation"
-                    value = "horizontal"
-                    checked = "checked" >
+            {{-- flat  roof left --}}
+            <div class = "mx-5" >
+                <label
+                        for = "vertical"
+                        class = "text-right self-center" >
+                    <img
+                            src = "{{asset('images/hall-parameters/vertical.svg')}}"
+                            alt = "{{trans("t_h_p.text.flat_roof_left")}}" >
+                </label >
+                <input
+                        type = "radio"
+                        id = "vertical"
+                        class = "self-center"
+                        name = "cladding_orientation"
+                        value = "vertical" >
+            </div >
+
         </div >
 
-        {{-- flat  roof left --}}
-        <div class = "mx-5" >
-            <label
-                    for = "vertical"
-                    class = "text-right self-center" >
-                <img
-                        src = "{{asset('images/hall-parameters/vertical.svg')}}"
-                        alt = "{{trans("t_h_p.text.flat_roof_left")}}" >
-            </label >
-            <input
-                    type = "radio"
-                    id = "vertical"
-                    class = "self-center"
-                    name = "cladding_orientation"
-                    value = "vertical" >
-        </div >
 
-    </div >
+    </x-slot>
 
-</fieldset >
+</x-hall-parameters::helpers.form-fieldset>
