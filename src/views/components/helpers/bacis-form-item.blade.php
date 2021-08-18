694ecfd7dfe10b5950f0424d29cf217d38ec930b
ljanu <info@gamatall.cz>
2021-08-18 11:00:08 +0200

    new component badic form

---
+++ src/views/components/helpers/bacis-form-item.blade.php
@@ -0,0 +1,16 @@
+{{-- component badic form item --}}
+
+<div class = "m-base md:mt-2 md:flex md:flex-row" >
+    <div class = "text-left md:text-right self-center flex-1" >
+
+        {{$label}}
+
+    </div >
+
+    <div class = "flex-1" >
+
+        {{$input}}
+
+    </div >
+
+</div >
