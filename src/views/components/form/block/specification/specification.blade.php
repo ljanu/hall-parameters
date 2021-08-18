a8c6f07fa53dedaf48d3cdb1c72b4607db30d8a7
ljanu <info@gamatall.cz>
2021-08-18 11:41:51 +0200

    added components

--- src/views/components/form/block/specification/specification.blade.php
+++ src/views/components/form/block/specification/specification.blade.php
@@ -1,48 +1,66 @@
 {{-- component specification --}}
 
-<fieldset class = "m-base" >
-
-    <legend class = "m-title" >{{trans("t_h_p.text.specification")}}</legend >
-
-    {{-- wrap basic items --}}
-    <div class = "grid grid-cols-2" >
-
+<x-hall-parameters::helpers.form-fieldset
+:legend="trans('t_h_p.text.specification')">
+    
+    <x-slot name="content">
 
         {{-- name object --}}
-        <label
-                for = "name"
-                class = "text-right self-center" > {{trans("t_h_p.text.name_object")}}
-        </label >
+        <x-hall-parameters::helpers.form-item>
 
-        <input
-                type = "text"
-                id = "name"
-                name = "name"
-                autofocus
-                class = "w-56"
-                value = ""
-        >
+            <x-slot name="label">
 
+                <label for = "name">
+                    {{trans("t_h_p.text.name_object")}}
+                </label >
+
+            </x-slot>
+
+            <x-slot name="input">
+
+                <input
+                        type = "text"
+                        id = "name"
+                        name = "name"
+                        autofocus
+                        value = ""
+                        class="w-full"
+                >
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
 
         {{-- roof load --}}
-        <label
-                for = "roof_load"
-                class = "text-right self-center" > {{trans("t_h_p.text.roof_load")}}
-        </label >
+        <x-hall-parameters::helpers.form-item>
 
-        <select
-                name = "roof_load"
-                id = "roof_load"
-                class = "w-56"
-        >
-            <option value = "1" >{{trans("t_h_p.text.load")}} 100 kg/m2</option >
-            <option value = "2" >{{trans("t_h_p.text.load")}} 200 kg/m2</option >
-            <option value = "3" >{{trans("t_h_p.text.load")}} 300 kg/m2</option >
-            <option value = "4" >{{trans("t_h_p.text.load")}} 400 kg/m2</option >
-            <option value = "5" >{{trans("t_h_p.text.load")}} 500 kg/m2</option >
-            <option value = "6" >{{trans("t_h_p.text.load")}} 600 kg/m2</option >
-        </select >
+            <x-slot name="label">
 
-    </div >
+                <label for = "roof_load">
+                    {{trans("t_h_p.text.roof_load")}}
+                </label >
 
-</fieldset >
+            </x-slot>
+
+            <x-slot name="input">
+
+                <select
+                        name = "roof_load"
+                        class="w-full"
+                        id = "roof_load"
+                >
+                    <option value = "1" >{{trans("t_h_p.text.load")}} 100 kg/m2</option >
+                    <option value = "2" >{{trans("t_h_p.text.load")}} 200 kg/m2</option >
+                    <option value = "3" >{{trans("t_h_p.text.load")}} 300 kg/m2</option >
+                    <option value = "4" >{{trans("t_h_p.text.load")}} 400 kg/m2</option >
+                    <option value = "5" >{{trans("t_h_p.text.load")}} 500 kg/m2</option >
+                    <option value = "6" >{{trans("t_h_p.text.load")}} 600 kg/m2</option >
+                </select >
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+    </x-slot>
+
+</x-hall-parameters::helpers.form-fieldset>
