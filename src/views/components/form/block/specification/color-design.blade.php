f9fbf3947aa8d5f88b0779854d1fc1a3d008d5ac
ljanu <info@gamatall.cz>
2021-08-19 11:42:43 +0200

    first item

--- src/views/components/form/block/specification/color-design.blade.php
+++ src/views/components/form/block/specification/color-design.blade.php
@@ -1,6 +1,44 @@
 {{-- component color design --}}
+@props([
+    'colorsCladding' => config('config_hall.ral_base')
+] )
 
-@php($colorsCladding = config('config_hall.ral_base'))
+<x-hall-parameters::helpers.form-fieldset 
+        :legend="trans('t_h_p.text.basic_color_design')">
+    
+    <x-slot name="content">
+       
+       {{-- roof --}}
+        <x-hall-parameters::helpers.form-item>
+            
+            <x-slot name="label">
+                <label
+                        for = "color_roof"
+                        class = "text-right self-center" >{{trans("t_h_p.text.color_roof")}}
+                </label >
+            </x-slot>
+            
+            <x-slot name="input">
+                <select
+                        name = "color_roof"
+                        id = "color_roof"
+                        class = "w-56"
+                >
+                    @foreach ($colorsCladding as $color)
+                        <option
+                                class = "{{'ral-'.$color}}"
+                                value = "{{$color}}" >RAL {{$color}}</option >
+                    @endforeach
+                </select >
+            </x-slot>
+        </x-hall-parameters::helpers.form-item>
+
+
+    </x-slot>
+    
+</x-hall-parameters::helpers.form-fieldset>
+
+
 
 <fieldset class = "m-base" >
 
