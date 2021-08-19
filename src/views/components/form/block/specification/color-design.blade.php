434d8e0f5b5ecd59554ddf04691d75dbc3b48dad
ljanu <info@gamatall.cz>
2021-08-19 12:21:56 +0200

    clear code

--- src/views/components/form/block/specification/color-design.blade.php
+++ src/views/components/form/block/specification/color-design.blade.php
@@ -4,20 +4,20 @@
 ] )
 
 <x-hall-parameters::helpers.form-fieldset
-        :legend = "trans('t_h_p.text.basic_color_design')" >
+        :legend="trans('t_h_p.text.basic_color_design')">
 
-    <x-slot name = "content" >
+    <x-slot name="content">
 
-        {{-- roof --}}
-        <x-hall-parameters::helpers.form-item >
+       {{-- roof --}}
+        <x-hall-parameters::helpers.form-item>
 
-            <x-slot name = "label" >
-                <label for = "color_roof" >
+            <x-slot name="label">
+                <label for = "color_roof">
                     {{trans("t_h_p.text.color_roof")}}
                 </label >
-            </x-slot >
+            </x-slot>
 
-            <x-slot name = "input" >
+            <x-slot name="input">
                 <select
                         name = "color_roof"
                         id = "color_roof"
@@ -29,20 +29,20 @@
                                 value = "{{$color}}" >RAL {{$color}}</option >
                     @endforeach
                 </select >
-            </x-slot >
+            </x-slot>
 
-        </x-hall-parameters::helpers.form-item >
+        </x-hall-parameters::helpers.form-item>
 
         {{-- walls --}}
-        <x-hall-parameters::helpers.form-item >
+        <x-hall-parameters::helpers.form-item>
 
-            <x-slot name = "label" >
-                <label for = "color_walls" >
+            <x-slot name="label">
+                <label for = "color_walls">
                     {{trans("t_h_p.text.color_walls")}}
                 </label >
-            </x-slot >
+            </x-slot>
 
-            <x-slot name = "input" >
+            <x-slot name="input">
                 <select
                         name = "color_walls"
                         id = "color_walls"
@@ -54,20 +54,20 @@
                                 value = "{{$color}}" >RAL {{$color}}</option >
                     @endforeach
                 </select >
-            </x-slot >
+            </x-slot>
 
-        </x-hall-parameters::helpers.form-item >
+        </x-hall-parameters::helpers.form-item>
 
         {{-- fill --}}
-        <x-hall-parameters::helpers.form-item >
+        <x-hall-parameters::helpers.form-item>
 
-            <x-slot name = "label" >
-                <label for = "color_fill" >
+            <x-slot name="label">
+                <label for = "color_fill">
                     {{trans("t_h_p.text.color_fill")}}
                 </label >
-            </x-slot >
+            </x-slot>
 
-            <x-slot name = "input" >
+            <x-slot name="input">
                 <select
                         name = "color_fill"
                         id = "color_fill"
@@ -80,19 +80,19 @@
                     @endforeach
                 </select >
 
-            </x-slot >
-        </x-hall-parameters::helpers.form-item >
+            </x-slot>
+        </x-hall-parameters::helpers.form-item>
 
         {{-- edges --}}
-        <x-hall-parameters::helpers.form-item >
+        <x-hall-parameters::helpers.form-item>
 
-            <x-slot name = "label" >
-                <label for = "color_edges" >
+            <x-slot name="label">
+                <label for = "color_edges">
                     {{trans("t_h_p.text.color_edges")}}
                 </label >
-            </x-slot >
+            </x-slot>
 
-            <x-slot name = "input" >
+            <x-slot name="input">
                 <select
                         name = "color_edges"
                         id = "color_edges"
@@ -104,33 +104,32 @@
                                 value = "{{$color}}" >RAL {{$color}}</option >
                     @endforeach
                 </select >
-            </x-slot >
 
-        </x-hall-parameters::helpers.form-item >
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
 
         {{-- construction --}}
-        <x-hall-parameters::helpers.form-item >
+        <x-hall-parameters::helpers.form-item>
 
-            <x-slot name = "label" >
-                <label for = "color_construction" >
+            <x-slot name="label">
+                <label for = "color_construction">
                     {{trans("t_h_p.text.color_construction")}}
                 </label >
-            </x-slot >
+            </x-slot>
 
-            <x-slot name = "input" >
+            <x-slot name="input">
                 <select
                         name = "color_construction"
                         id = "color_construction"
                         class = "w-56"
                 >
-                    <option
-                            value = "7035" >RAL 7038
-                    </option >
+                    <option value = "7035" >RAL 7038</option >
                 </select >
-            </x-slot >
+            </x-slot>
 
-        </x-hall-parameters::helpers.form-item >
+        </x-hall-parameters::helpers.form-item>
 
-    </x-slot >
+    </x-slot>
 
-</x-hall-parameters::helpers.form-fieldset >
+</x-hall-parameters::helpers.form-fieldset>
