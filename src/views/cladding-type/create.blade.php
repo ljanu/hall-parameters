6c6b95f68e291cc8a3c7c14b659b54e5c546fecd
ljanu <info@gamatall.cz>
2021-08-19 12:59:32 +0200

    added translation

--- src/views/cladding-type/create.blade.php
+++ src/views/cladding-type/create.blade.php
@@ -1,12 +1,12 @@
 <x-hall-parameters::welcome_hall_parameters>
     <x-slot name="content">
 
-        <h1>Typ opláštění</h1>
+        <h1>{{trans("t_h_p.text.cladding_type")}}</h1>
 
         <div class="text-center">{{trans("t_h_p.text.new")}}</div>
 
         {{-- form cladding type --}}
-<x-hall-parameters::form.cladding-type method="post"/>
+        <x-hall-parameters::form.cladding-type method="post"/>
 
 
     </x-slot>
