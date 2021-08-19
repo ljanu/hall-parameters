6fe66c9890018a4af465067985b65e73b052c953
ljanu <info@gamatall.cz>
2021-08-19 12:12:33 +0200

    added component

--- src/views/components/form/block/specification/color-design.blade.php
+++ src/views/components/form/block/specification/color-design.blade.php
@@ -3,22 +3,21 @@
     'colorsCladding' => config('config_hall.ral_base')
 ] )
 
-<x-hall-parameters::helpers.form-fieldset 
-        :legend="trans('t_h_p.text.basic_color_design')">
-    
-    <x-slot name="content">
-       
-       {{-- roof --}}
-        <x-hall-parameters::helpers.form-item>
-            
-            <x-slot name="label">
-                <label
-                        for = "color_roof"
-                        class = "text-right self-center" >{{trans("t_h_p.text.color_roof")}}
+<x-hall-parameters::helpers.form-fieldset
+        :legend = "trans('t_h_p.text.basic_color_design')" >
+
+    <x-slot name = "content" >
+
+        {{-- roof --}}
+        <x-hall-parameters::helpers.form-item >
+
+            <x-slot name = "label" >
+                <label for = "color_roof" >
+                    {{trans("t_h_p.text.color_roof")}}
                 </label >
-            </x-slot>
-            
-            <x-slot name="input">
+            </x-slot >
+
+            <x-slot name = "input" >
                 <select
                         name = "color_roof"
                         id = "color_roof"
@@ -30,111 +29,108 @@
                                 value = "{{$color}}" >RAL {{$color}}</option >
                     @endforeach
                 </select >
-            </x-slot>
-        </x-hall-parameters::helpers.form-item>
+            </x-slot >
 
-
-    </x-slot>
-    
-</x-hall-parameters::helpers.form-fieldset>
-
-
-
-<fieldset class = "m-base" >
-
-    <legend class = "m-title" >{{trans("t_h_p.text.basic_color_design")}}</legend >
-
-    {{-- wrap colors --}}
-    <div class = "grid grid-cols-2 self-center" >
-
-
-        {{-- roof --}}
-        <label
-                for = "color_roof"
-                class = "text-right self-center" >{{trans("t_h_p.text.color_roof")}}
-        </label >
-
-        <select
-                name = "color_roof"
-                id = "color_roof"
-                class = "w-56"
-        >
-            @foreach ($colorsCladding as $color)
-                <option
-                        class = "{{'ral-'.$color}}"
-                        value = "{{$color}}" >RAL {{$color}}</option >
-            @endforeach
-        </select >
-
+        </x-hall-parameters::helpers.form-item >
 
         {{-- walls --}}
-        <label
-                for = "color_walls"
-                class = "text-right self-center" >{{trans("t_h_p.text.color_walls")}}
-        </label >
-        <select
-                name = "color_walls"
-                id = "color_walls"
-                class = "w-56"
-        >
-            @foreach ($colorsCladding as $color)
-                <option
-                        class = "{{'ral-'.$color}}"
-                        value = "{{$color}}" >RAL {{$color}}</option >
-            @endforeach
-        </select >
+        <x-hall-parameters::helpers.form-item >
 
+            <x-slot name = "label" >
+                <label for = "color_walls" >
+                    {{trans("t_h_p.text.color_walls")}}
+                </label >
+            </x-slot >
+
+            <x-slot name = "input" >
+                <select
+                        name = "color_walls"
+                        id = "color_walls"
+                        class = "w-56"
+                >
+                    @foreach ($colorsCladding as $color)
+                        <option
+                                class = "{{'ral-'.$color}}"
+                                value = "{{$color}}" >RAL {{$color}}</option >
+                    @endforeach
+                </select >
+            </x-slot >
+
+        </x-hall-parameters::helpers.form-item >
 
         {{-- fill --}}
-        <label
-                for = "color_fill"
-                class = "text-right self-center" >{{trans("t_h_p.text.color_fill")}}
-        </label >
-        <select
-                name = "color_fill"
-                id = "color_fill"
-                class = "w-56"
-        >
-            @foreach ($colorsCladding as $color)
-                <option
-                        class = "{{'ral-'.$color}}"
-                        value = "{{$color}}" >RAL {{$color}}</option >
-            @endforeach
-        </select >
+        <x-hall-parameters::helpers.form-item >
 
+            <x-slot name = "label" >
+                <label for = "color_fill" >
+                    {{trans("t_h_p.text.color_fill")}}
+                </label >
+            </x-slot >
+
+            <x-slot name = "input" >
+                <select
+                        name = "color_fill"
+                        id = "color_fill"
+                        class = "w-56"
+                >
+                    @foreach ($colorsCladding as $color)
+                        <option
+                                class = "{{'ral-'.$color}}"
+                                value = "{{$color}}" >RAL {{$color}}</option >
+                    @endforeach
+                </select >
+
+            </x-slot >
+        </x-hall-parameters::helpers.form-item >
 
         {{-- edges --}}
-        <label
-                for = "color_edges"
-                class = "text-right self-center" >{{trans("t_h_p.text.color_edges")}}
-        </label >
-        <select
-                name = "color_edges"
-                id = "color_edges"
-                class = "w-56"
-        >
-            @foreach ($colorsCladding as $color)
-                <option
-                        class = "{{'ral-'.$color}}"
-                        value = "{{$color}}" >RAL {{$color}}</option >
-            @endforeach
-        </select >
+        <x-hall-parameters::helpers.form-item >
 
+            <x-slot name = "label" >
+                <label for = "color_edges" >
+                    {{trans("t_h_p.text.color_edges")}}
+                </label >
+            </x-slot >
+
+            <x-slot name = "input" >
+                <select
+                        name = "color_edges"
+                        id = "color_edges"
+                        class = "w-56"
+                >
+                    @foreach ($colorsCladding as $color)
+                        <option
+                                class = "{{'ral-'.$color}}"
+                                value = "{{$color}}" >RAL {{$color}}</option >
+                    @endforeach
+                </select >
+            </x-slot >
+
+        </x-hall-parameters::helpers.form-item >
 
         {{-- construction --}}
-        <label
-                for = "color_construction"
-                class = "text-right self-center" >{{trans("t_h_p.text.color_construction")}}
-        </label >
-        <select
-                name = "color_construction"
-                id = "color_construction"
-                class = "w-56"
-        >
-            <option
-                    value = "7035" >RAL 7038</option >
-        </select >
+        <x-hall-parameters::helpers.form-item >
 
-    </div >
+            <x-slot name = "label" >
+                <label for = "color_construction" >
+                    {{trans("t_h_p.text.color_construction")}}
+                </label >
+            </x-slot >
 
-</fieldset >
+            <x-slot name = "input" >
+                <select
+                        name = "color_construction"
+                        id = "color_construction"
+                        class = "w-56"
+                >
+                    <option
+                            value = "7035" >RAL 7038
+                    </option >
+                </select >
+            </x-slot >
+
+        </x-hall-parameters::helpers.form-item >
+
+    </x-slot >
+
+</x-hall-parameters::helpers.form-fieldset >
