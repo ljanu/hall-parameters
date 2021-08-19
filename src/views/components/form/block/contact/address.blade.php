011595882068980ac1851d7d586922455e38bf6c
ljanu <info@gamatall.cz>
2021-08-19 14:20:59 +0200

    base layout

--- src/views/components/form/block/contact/address.blade.php
+++ src/views/components/form/block/contact/address.blade.php
@@ -1,10 +1,122 @@
 {{-- component address contact --}}
+<x-hall-parameters::helpers.form-fieldset :legend="trans('t_h_p.text.address')">
+    <x-slot name="content">
 
-<fieldset class="m-base grid grid-cols-2" >
-    <legend class="m-title" >{{trans("t_h_p.text.address")}}</legend >
+        {{-- address line 1 --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- address line 2 --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- city --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- zip code --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- region --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- distance --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- plot number --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+        {{-- country --}}
+        <x-hall-parameters::helpers.form-item>
+
+            <x-slot name="label">
+
+            </x-slot>
+
+            <x-slot name="input">
+
+            </x-slot>
+
+        </x-hall-parameters::helpers.form-item>
+
+    </x-slot>
+</x-hall-parameters::helpers.form-fieldset>
+
+
+<fieldset class = "m-base grid grid-cols-2" >
+    <legend class = "m-title" >{{trans("t_h_p.text.address")}}</legend >
 
     {{-- address line 1 --}}
-    <label class="text-right self-center" for = "address_line_1" > {{trans("t_h_p.text.address_line_1")}}</label >
+    <label
+            class = "text-right self-center"
+            for = "address_line_1" > {{trans("t_h_p.text.address_line_1")}}</label >
 
     <input
             type = "text"
